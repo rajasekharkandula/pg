c36660ad3de7f0de05f2284ab4e1f1f87bc3b8f0
Raja <rajasekhar.janani@gmail.com>
2017-05-27 14:48:54 +0530

    Db COnfig

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['hostname'] = '166.62.10.65';
+$db['default']['username'] = 'edujana';
+$db['default']['password'] = 'Edujana@23';
 $db['default']['database'] = 'painlessgift';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
