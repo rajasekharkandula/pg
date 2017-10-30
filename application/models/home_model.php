da9f7b06fcac44519b33acf3c3bf78e9a0da47f1
Raja <rajasekhar.janani@gmail.com>
2017-10-30 22:57:04 +0530

    Checking

--- application/models/home_model.php
+++ application/models/home_model.php
@@ -822,14 +822,18 @@ class Home_model extends CI_Model{
 		
 		#Send Reponse To FireBase Server	
 		$ch = curl_init();
-		curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
-		curl_setopt( $ch,CURLOPT_POST, true );
-		curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
-		curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
-		curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
-		curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
-		$result = curl_exec($ch );
-		curl_close( $ch );
+		curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);	
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+		$result = curl_exec($ch);				
+		if ($result === FALSE) {
+		die('Curl failed: ' . curl_error($ch));
+		}
+		curl_close($ch);
 
 		#Echo Result Of FireBase Server
 		//echo $result;exit();
